a1edd0fabe30cce458748e627c2cb2dee8b56900
David Paquette <david.paquette@usask.ca>
2015-11-05 11:25:55 -0800

    Refactored modal tag helper for better composeability

--- TagHelperSamples/src/TagHelperSamples.Bootstrap/ModalTagHelper.cs
+++ TagHelperSamples/src/TagHelperSamples.Bootstrap/ModalTagHelper.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+﻿using Microsoft.AspNet.Html.Abstractions;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
 using System.Threading.Tasks;
 
 namespace TagHelperSamples.Bootstrap
 {
+    public class ModalContext
+    {
+        public IHtmlContent Body { get; set; }
+        public IHtmlContent Footer { get; set; }
+    }
+
     /// <summary>
     /// A Bootstrap modal dialog
     /// </summary>
+    [RestrictChildren("modal-body", "modal-footer")]
     public class ModalTagHelper : TagHelper
     {
         /// <summary>
@@ -20,8 +29,11 @@ namespace TagHelperSamples.Bootstrap
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            var childContent = await context.GetChildContentAsync();
-            var content = childContent.GetContent();
+            var modalContext = new ModalContext();
+            context.Items.Add(typeof(ModalTagHelper), modalContext);
+
+            await context.GetChildContentAsync();
+
             var template =
 $@"<div class='modal-dialog' role='document'>
     <div class='modal-content'>
@@ -29,9 +41,8 @@ $@"<div class='modal-dialog' role='document'>
         <button type = 'button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
         <h4 class='modal-title' id='{context.UniqueId}Label'>{Title}</h4>
       </div>
-        {content}
-    </div>
-  </div>";
+        <div class='modal-body'>";
+
             output.TagName = "div";
             output.Attributes["role"] = "dialog";
             output.Attributes["id"] = Id;
@@ -43,8 +54,20 @@ $@"<div class='modal-dialog' role='document'>
                 classNames = string.Format("{0} {1}", output.Attributes["class"].Value, classNames);
             }
             output.Attributes["class"] = classNames;
-
-            output.Content.SetContentEncoded(template);
+            output.Content.AppendEncoded(template);
+            if (modalContext.Body != null)
+            {
+                output.Content.Append(modalContext.Body);
+            }
+            output.Content.AppendEncoded("</div>");
+            if (modalContext.Footer != null)
+            {
+                output.Content.AppendEncoded("<div class='modal-footer'>");
+                output.Content.Append(modalContext.Footer);
+                output.Content.AppendEncoded("</div>");
+            }
+            
+            output.Content.AppendEncoded("</div></div>");
         }
     }
 }
