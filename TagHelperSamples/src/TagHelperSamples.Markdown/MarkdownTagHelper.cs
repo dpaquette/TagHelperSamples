9a66c20668486d725387e17b6bbfa58396740334
Maxime Rouiller <angelzerosoft@gmail.com>
2015-11-05 08:59:46 -0800

    Fixed issue #19. Added support to CoreClr.

--- TagHelperSamples/src/TagHelperSamples.Markdown/MarkdownTagHelper.cs
+++ TagHelperSamples/src/TagHelperSamples.Markdown/MarkdownTagHelper.cs
@@ -1,25 +1,35 @@
-﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using CommonMark;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
 
 namespace TagHelperSamples.Markdown
 {
-    [HtmlTargetElement("p", Attributes = "markdown")]
-    [HtmlTargetElement("markdown")]
-    [OutputElementHint("p")]
+    [HtmlTargetElement("markdown", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement(Attributes = "markdown")]
     public class MarkdownTagHelper : TagHelper
     {
+        public ModelExpression Content { get; set; }
         public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (output.TagName == "markdown")
             {
-                output.TagName = "p";
+                output.TagName = null;
             }
-            var childContent = await context.GetChildContentAsync();
-            var markdownContent = childContent.GetContent();
+            output.Attributes.RemoveAll("markdown");
 
-            var markdown = new MarkdownSharp.Markdown();
-            var htmlContent = markdown.Transform(markdownContent);
-            output.Content.SetContentEncoded(htmlContent);
+            var content = await GetContent(context);
+            var markdown = content;
+            var html = CommonMarkConverter.Convert(markdown ?? "");
+            output.Content.SetContentEncoded(html ?? "");
+        }
+
+        private async Task<string> GetContent(TagHelperContext context)
+        {
+            if (Content == null)
+                return (await context.GetChildContentAsync()).GetContent();
+
+            return Content.Model?.ToString();
         }
     }
 }
